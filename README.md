2d5e7d511e0acf31378677886b2e2736e474b735
Thiago Luna <thiagoluna2@gmail.com>
2024-06-30 20:02:59 -0300

    feat: hello world

---
+++ README.md
@@ -0,0 +1,11 @@
+<h2 align="center">
+  Blockchain Projects developed with Solidity
+</h2>
+<br>  
+## 💻 Projects
+
+This repo contain projects developed with Solidity.  
+Metamask with BNB Facet-test Account  
+Remix-Ethereum IDE
+
+Developed by Thiago Luna - [Linkedin](https://www.linkedin.com/in/thiago-luna/)
---
+++ hello-world-remix/HelloWorld.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract HelloWorld {
+    string public message = "Hello World";
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
